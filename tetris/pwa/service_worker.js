dce2cae2a9a1322fc06d4744292aea55938d892e
hikaru88 <hikaru8705@gmail.com>
2019-12-23 20:51:00 +0900

    test

---
+++ tetris/pwa/service_worker.js
@@ -0,0 +1,37 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/poster-keisuke.github.io/',
+];
+
+// service workerの記述
+    if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service_worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+    });
+    }
+    </script>
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// キャッシュロードの処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
